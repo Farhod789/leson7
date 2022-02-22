c398299fa71a9702298b14063062ad8875b9bf3e
Feday <farhodeshkuvvatov@gmail.com>
2022-02-22 18:59:15 +0500

    my first commit

---
+++ main6.js
@@ -0,0 +1,13 @@
+// const obj = {
+//     name: 'Vanya',
+//     age: 25,
+//     ismarried: false
+// };
+// console.log(obj['name']);
+
+// const result = confirm('eto pravda?');  //  oprosnik true ili false
+// console.log(result)
+
+// const answer = prompt('vam est 18');  // oprosnik voimom textom
+// console.log(answer);
+
---
+++ main9.js
@@ -0,0 +1,12 @@
+/* fail obmen GIT 
+git config --global user.name "roor roro"
+git config --global user.email example@email.com
+
+git status esli faili budet --krasnogo-- sveta oni ewe ne dobaleni esli --zelenogo sveta to dobavleni
+
+git add -A dobavlyaet faili dlya nablyudenie esli nujno dobavit tolko css ili js nujno (*.css ili *.js)
+
+esli nujno commit  nujno
+git commit -a -m"txtjf jkf"   / a - eto all  m - eto message
+*/
+
